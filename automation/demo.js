99a2cb8296bb73bb69363333e5b33933be2e03fa
yakimax <sv1099912@gmal.com>
2022-02-28 19:17:38 +0530

    commit

---
+++ automation/demo.js
@@ -0,0 +1,96 @@
+const loginLink = "https://www.hackerrank.com/auth/login";
+const puppeteer = require("puppeteer");
+
+const codeFile = require('./code')
+
+console.log("Before");
+
+let email = "novovi8175@robhung.com";
+let password = "pepcoding123";
+
+let page;
+
+let browserWillLaunchPromise = puppeteer.launch({
+  headless: false,
+  args: ["--start-maximized"],
+  defaultViewport: null,
+}); // launch returns a promise to open Browser
+
+browserWillLaunchPromise
+  .then(function (browserInstance) {
+    let newTabPromise = browserInstance.newPage();
+    return newTabPromise;
+    // new Page method will open a new Tab in the Browser
+  })
+  .then(function (newTab) {
+    page = newTab;
+    let websiteWillbeOpenedPromise = newTab.goto(loginLink);
+    return websiteWillbeOpenedPromise;
+  })
+  .then(function () {
+    let emailWillbeEnteredPromise = page.type("input[id='input-1']", email, {
+      delay: 100,
+    });
+    return emailWillbeEnteredPromise;
+  })
+  .then(function () {
+    let passwordWillbeEnteredPromise = page.type(
+      "input[id=input-2]",
+      password,
+      { delay: 100 }
+    );
+    return passwordWillbeEnteredPromise;
+  })
+  .then(function () {
+    let loginButtonClickPromise = page.click(
+      'button[data-analytics="LoginPassword"]',
+      { delay: 50 }
+    );
+    return loginButtonClickPromise;
+  })
+  .then(function () {
+    let algoSecClickedPromise = waitAndClick('a[data-attr1="algorithms"]', page);
+    return algoSecClickedPromise;
+  })
+  .then(function () {
+    let warmUpSectionClickedPromise = waitAndClick('input[value="warmup"]' , page)
+    return warmUpSectionClickedPromise;
+  }).then(function(){
+      let allQuestionsArrPromise = page.$$('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled')
+      return allQuestionsArrPromise;
+  }).then(function(TotalQuestions){
+    console.log('Number of Questions -> ' + TotalQuestions.length)
+    let questionWillBeSolved = questionSolver(page , TotalQuestions[0] , codeFile.answers[0])
+    return questionWillBeSolved
+  });
+
+function waitAndClick(selector, cPage) {
+  return new Promise(function (resolve, reject) {
+    let waitForModalPromise = cPage.waitForSelector(selector);
+    waitForModalPromise
+      .then(function () {
+        let clickModal = cPage.click(selector, { delay: 100 });
+        return clickModal;
+      })
+      .then(function () {
+        resolve();
+      })
+      .catch(function () {
+        reject();
+      });
+  });
+}
+
+
+function questionSolver(page , question , answer){
+
+  return new Promise(function(resolve , reject){
+    let questionWillbeClickedPromise = question.click()
+    questionWillbeClickedPromise.then(function(){
+      console.log('Question is Clicked')
+    })
+  })
+
+}
+
+console.log("After");
