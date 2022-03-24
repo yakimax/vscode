7f28a010d625ee3cee8bd77f756b22a7ac1d8d76
yakimax <sv1099912@gmal.com>
2022-03-24 14:15:06 +0530

    half

--- jira_Clone/script.js
+++ jira_Clone/script.js
@@ -1,4 +1,17 @@
 let addBtnClick = document.querySelector('.add-button')
-addBtnClick.addEventListener('click',function(){
-    console.log('clicked');
+let modalCont = document.querySelector('.modal-cont')
+let flag = false
+addBtnClick.addEventListener('click',function(e){
+
+    //display box
+    flag = !flag;
+    if(falg==true){
+        modalCont.style.display='flex'
+
+     
+    }
+    else{
+        modalCont.style.display='none'
+    }
 });
+
