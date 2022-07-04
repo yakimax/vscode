7922de9d8beeec6c2b52fd566200f7349146d3d5
yakimax <sv1099912@gmail.com>
2022-07-04 22:45:20 +0530

    commit

--- java/oops/Main.java
+++ java/oops/Main.java
@@ -1,7 +1,7 @@
 package oops;
 public class Main {
     
-    public class Point{ 
+    public static  class Point{ 
         int x ;
         String y ;
     }
@@ -12,10 +12,11 @@ public class Main {
         Point bl ;
         Point br ;
         void print(){
-            System.out.println( tl.x , tl.y , tr.x , tl.y , bl.x , bl.y , br.x , br.y ) ;
+            System.out.println( tl.x + tl.y + tr.x + tl.y + bl.x + bl.y + br.x + br.y ) ;
         }
     }
     public static void main(String[] args) {
-        
+        Point p = new Point();
+        System.out.println( p + "      " + p.hashCode());
     }
 }
