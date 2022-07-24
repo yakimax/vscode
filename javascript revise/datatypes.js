91d0feeac656e553c15f47b1e1ccc283fa5a9193
yakimax <sv1099912@gmail.com>
2022-07-24 19:14:26 +0530

    commit

---
+++ javascript revise/datatypes.js
@@ -0,0 +1,38 @@
+var variable = 'a' ;
+
+
+console.log(variable) ;
+
+var variable = "String" ;
+
+
+console.log(variable) ;
+
+
+
+let variable = 1 ;
+
+
+
+console.log(variable);
+
+
+
+let variable = 1.22;
+
+
+console.log(variable);
+
+
+// let var = react ;
+
+
+
+console.log(variable);
+
+
+
+
+let variable = null ;
+
+console.log(variable);
