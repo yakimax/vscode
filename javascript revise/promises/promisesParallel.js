b5aed786245cf5318ca4c7c7589f6fc71f7e8e16
yakimax <sv1099912@gmail.com>
2022-08-02 11:51:12 +0530

    add

--- javascript revise/promises/promisesParallel.js
+++ javascript revise/promises/promisesParallel.js
@@ -1,7 +1,9 @@
 let fs = require('fs') ;
 
 
+
 console.log('before') ;
+
 
 let f1p = fs.promises.readFile('./f1.txt') ;
 
@@ -10,17 +12,23 @@ let f2p = fs.promises.readFile('./f2.txt') ;
 let f3p = fs.promises.readFile('./f3.txt') ;
 
 
+
 f1p.then(function(data){
     console.log(data + " ") ;
 }) ;
+
+
 
 f2p.then(function(data){
     console.log(data + " ") ;
 }) ;
 
+
+
 f3p.then(function(data){
     console.log(data + " ") ;
 }) ;
+
 
 
 console.log('after') ;
