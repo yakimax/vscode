ac50a921c5b03432aad5afd422a639902905f1bd
yakimax <sv1099912@gmail.com>
2022-08-07 23:56:10 +0530

    ADD

---
+++ javascript revise/classesInJavascript/ConstructorFunctions.js
@@ -0,0 +1,11 @@
+// constructor functions in javascript
+
+function Car(name,id,height){ 
+    this.Name = name ;
+    this.Height = height ;
+    this.Id = id ;
+}
+
+let car1 = new Car('shivam',69,6.1);
+
+console.log(car1) ;
